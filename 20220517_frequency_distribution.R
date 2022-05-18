2a32a0278a43b11959b7bee88c7d3daba5ef3b73
tres871211 <0021321@gmail.com>
2022-05-18 14:48:01 +0800

    Record point

--- 20220517_frequency_distribution.R
+++ 20220517_frequency_distribution.R
@@ -42,9 +42,9 @@
         y=..count../sum(..count..))
 
   ## Beautify plot
-    p_Den <- ggplot(FreDis.df, aes(x = Area, colour = Groups, group = Samples)) + geom_density()
-    p_Den
-    p_Den + theme_classic() + # White background
+    P_Den <- ggplot(FreDis.df, aes(x = Area, colour = Groups, group = Samples)) + geom_density()
+    P_Den
+    P_Den + theme_classic() + # White background
             theme(axis.line = element_line(colour = "black", 
                                             size = 1, linetype = "solid")) + # Change the line type and color of axis lines
             theme(axis.text.x = element_text(face="bold", color="black", 
@@ -63,7 +63,7 @@
             theme(legend.text = element_text(colour="black", size=12, face="bold")) + # legend labels
             theme(legend.position = c(0.8, 0.8)) + #  legend.position 
             theme(legend.key.size = unit(1, 'cm')) -> p_Den2
-    p_Den2
+    P_Den2
     
     #### Basic dotplot ####
     P_Dot <-  ggplot(FreDis.df, aes(x=Samples, y= Area)) + geom_point()
@@ -88,6 +88,18 @@
       labs(fill="")
     P_His3
   
+    #### Overlay different type of Figures ####
+    ggplot(FreDis.df, aes(x=Area,  fill=Samples))  +
+      geom_density(size = 0.8, alpha = 0.5) + 
+      geom_histogram( aes(y = ..density..), color="#e9ecef", alpha=0.3, position = 'identity', binwidth = 400)
+    
+    ##
+    FreDis_S.df <- FreDis.df[FreDis.df$Samples=="F88",]
+    P_OvDenHis_S <- ggplot(data = FreDis_S.df,
+                    mapping = aes(x = Area, fill=as.factor(Samples)))
+    P_OvDenHis_S + geom_histogram(mapping = aes(y = ..density..), alpha = 0.5, binwidth=400) +
+                   geom_density(size = 0.8, alpha = 0.4)
+    rm(FreDis_S.df)  
 ##### Density Plot #####  
 
 ############################################################################################################################
@@ -96,110 +108,61 @@
 
 
 
-
-
-
-# ## 
-# library(UsingR)
-# hist(Galton$parent, freq = FALSE)
-# x <- seq(64, 74, length.out=100)
-# y <- with(Galton, dnorm(x, mean(parent), sd(parent)))
-# lines(x, y, col = "red")
-
-
-## 
-p <- ggplot(data = FreDis.df,  mapping = aes(
-              x = Area, 
-              color = Samples))
-p + geom_line(stat = "density")
-
-##
-dfs <- FreDis.df[FreDis.df$Samples=="F88",]
-p <- ggplot(data = dfs,
-            mapping = aes(x = Area, fill=as.factor(Samples)))
-p + geom_histogram(mapping = aes(y = ..density..), alpha = 0.4, binwidth=400) +
-  geom_density(size = 1.1, alpha = 0.6)
-
-############################################################################################################################
-
-## KLC
-# generate dataframe for ploting  
-to_plot <- data.frame(
-  Distance = output.df$`mean(Distance)`,
-  Density = output.df$Frequency/sum(output.tb$Frequency)
-) %>% unique()
-
-plt <- to_plot %>%
-  ggplot(aes(x=Distance, y=Density)) +
-  geom_smooth(method = "gam", se=TRUE, fill="#E0E0E0", color="red", size=0.5) +
-  geom_point(aes(x=Distance, y=Density), shape=21, color="#4F4F4F", fill="#4F4F4F", size=0.5)+
-  ggtitle("Full_data") +
-  ylim(0,0.03)+
-  geom_vline(xintercept = 1, color = "#5B5B5B", linetype="dotted", size=0.6)+
-  geom_vline(xintercept = 2, color = "#5B5B5B", linetype="dotted", size=0.6)+
-  theme_classic()
-plot(plt)
-
 ############################################################################################################################
 ## Main
-df <- MusFib.df
-df <- pivot_longer(df, cols = 1:ncol(df), 
-                   names_to = "Muscle_fiber_samples", values_to = "Muscle_fiber_size") %>%
-  left_join(Anno.df)
 
-df <- df[!is.na(df$Muscle_fiber_size),]
-MS_Max = df$Muscle_fiber_size  %>% max(na.rm = T)
-MS_Min = df$Muscle_fiber_size  %>% min(na.rm = T)
+
+FreDis.df <- FreDis.df[!is.na(FreDis.df$Area),]
+MS_Max = FreDis.df$Area  %>% max(na.rm = T)
+MS_Min = FreDis.df$Area  %>% min(na.rm = T)
 
 Bin = 400
 
-Max_Range = round(MS_Max/Bin)+1
+Max_Range = floor(MS_Max/Bin)+1 # floor(): Round down
 
-df$Range <- 1
+FreDis.df$Range <- 1
 for (i in 0:Max_Range+1) {
-  for (j in 1:nrow(df)) {
-    #if(df$Muscle_fiber_size[j] %in% range(i*400,(i+1)*400)){
-    if(i*Bin <= df$Muscle_fiber_size[j] && df$Muscle_fiber_size[j] < (i+1)*Bin ){
+  for (j in 1:nrow(FreDis.df)) {
+    #if(FreDis.df$Area[j] %in% range(i*400,(i+1)*400)){
+    if(i*Bin <= FreDis.df$Area[j] && FreDis.df$Area[j] < (i+1)*Bin ){
         
-      df$Range[j] <- i+1
+      FreDis.df$Range[j] <- i+1
     }else{
-      df$Range[j] <- df$Range[j]
+      FreDis.df$Range[j] <- FreDis.df$Range[j]
     }
-    
   }
-  
 }
 
 ## Anno 2
-Anno2.df <- data.frame(Muscle_fiber_samples = rep(Anno.df$Muscle_fiber_samples,each =max(df$Range)),
+Anno2.df <- data.frame(Samples = rep(Anno.df$Samples,each =max(df$Range)),
                                                   Range= seq(1:max(df$Range)))
 # 
 # dfTTT <- left_join(Anno2.df,df)
-# dfTTT[is.na(dfTTT[, "Muscle_fiber_size"]), "Muscle_fiber_size"] <- 0
+# dfTTT[is.na(dfTTT[, "Area"]), "Area"] <- 0
 # dfTTT <- left_join(dfTTT[,-4],Anno.df)
 # 
 # df <- dfTTT
 
-df2 <- df %>% group_by(Muscle_fiber_samples) %>% 
+df2 <- df %>% group_by(Samples) %>% 
               count(Range) %>% right_join(df)
 
 colnames(df2)[3] <- "Count" 
-df3 <- df2 %>% group_by(Muscle_fiber_samples) %>% 
-       count(Muscle_fiber_samples) %>% right_join(df2)
+df3 <- df2 %>% group_by(Samples) %>% 
+       count(Samples) %>% right_join(df2)
 df4 <- data.frame(df3, Fre=df3$Count/df3$n)
 
-df5 <- df4 %>% group_by(Range,Fre,Muscle_fiber_samples) %>% summarise(avg = mean(Muscle_fiber_size))
+df5 <- df4 %>% group_by(Range,Fre,Samples) %>% summarise(avg = mean(Area))
 df6 <- left_join(df5,Anno.df)
-# ggplot(df2, aes(x=avg, fill= Muscle_fiber_samples)) +
+# ggplot(df2, aes(x=avg, fill= Samples)) +
 #   geom_histogram() 
 
 #library(ggpubr)
 plt <- df6 %>%
   ggplot( mapping =aes(x=avg, y=Fre,color=Group,fill=Group ,
-                       group=Muscle_fiber_samples, shape= Muscle_fiber_samples))  +
+                       group=Samples, shape= Samples))  +
   geom_point(aes(x=avg, y=Fre, color=Group ))+
   scale_shape_manual(values = seq(1:nrow(Anno.df))) + # https://www.datanovia.com/en/blog/ggplot-point-shapes-best-tips/
-  #show_point_shapes(aes(x=avg, y=Fre, color=Group, shape= Muscle_fiber_samples ))+
+  #show_point_shapes(aes(x=avg, y=Fre, color=Group, shape= Samples ))+
   geom_smooth(se=F,method = "loess")+
   ggtitle("Full_data") # +
   # ylim(0,0.03)+
@@ -210,21 +173,21 @@ plt
 
 # https://stackoverflow.com/questions/71162252/adding-the-maximum-peak-value-in-ggplot-for-geom-smoth
 # create the smooth and retain rows with max of smooth, using slice_max
-sm_max = df6 %>% group_by(Muscle_fiber_samples) %>%
+sm_max = df6 %>% group_by(Samples) %>%
   mutate(smooth =predict(loess(Fre~as.numeric(avg), span=.5))) %>% 
   slice_max(order_by = smooth)
 
 ## Find max in center
-Deriv.df = df6 %>% group_by(Muscle_fiber_samples) %>%
+Deriv.df = df6 %>% group_by(Samples) %>%
   mutate(smooth =predict(loess(Fre~as.numeric(avg), span=.5))) 
-Muscle_fiber_samples.set <- Anno.df$Muscle_fiber_samples
+Samples.set <- Anno.df$Samples
 model <- smooth.spline(x = Deriv.df$avg, y = Deriv.df$smooth)
 Y1 <- predict(model, x = seq(0.1,max(Deriv.df$avg),length=1000), deriv=1)
 Y2 <- predict(model, x = seq(0.1,max(Deriv.df$avg),length=1000), deriv=2)
 
-for (i in 1:length(Muscle_fiber_samples.set)) {
+for (i in 1:length(Samples.set)) {
   if(i==1){
-    Deriv2.df <- Deriv.df[Deriv.df$Muscle_fiber_samples == Muscle_fiber_samples.set[i], ]
+    Deriv2.df <- Deriv.df[Deriv.df$Samples == Samples.set[i], ]
     model <- smooth.spline(x = Deriv2.df$avg, y = Deriv2.df$smooth)
     Y0 <- predict(model, x = seq(0.1,max(Deriv2.df$avg),length=1000), deriv=0)
     Y1 <- predict(model, x = seq(0.1,max(Deriv2.df$avg),length=1000), deriv=1)
@@ -239,7 +202,7 @@ for (i in 1:length(Muscle_fiber_samples.set)) {
       
     }
   }else{
-      DerivS.df <- Deriv.df[Deriv.df$Muscle_fiber_samples == Muscle_fiber_samples.set[i], ]
+      DerivS.df <- Deriv.df[Deriv.df$Samples == Samples.set[i], ]
       model <- smooth.spline(x = DerivS.df$avg, y = DerivS.df$smooth)
       Y0 <- predict(model, x = seq(0.1,max(DerivS.df$avg),length=1000), deriv=0)
       Y1 <- predict(model, x = seq(0.1,max(DerivS.df$avg),length=1000), deriv=1)
@@ -259,20 +222,20 @@ for (i in 1:length(Muscle_fiber_samples.set)) {
 
 rm(DerivS.df, DerivSR.df, model, Y0,Y1,Y2)
 
-Deriv_max.df <- left_join(sm_max,Deriv2.df[, c("Range", "Muscle_fiber_samples", "x","y")],by = c("Range", "Muscle_fiber_samples"))
+Deriv_max.df <- left_join(sm_max,Deriv2.df[, c("Range", "Samples", "x","y")],by = c("Range", "Samples"))
                 
 
 # Plot, using the same smooth as above (default is loess, span set at set above)
 # df6 %>%
-#   ggplot(df6, mapping = aes(avg,Fre,  group = Muscle_fiber_samples, color = Group)) +
+#   ggplot(df6, mapping = aes(avg,Fre,  group = Samples, color = Group)) +
 #   geom_point() +
 #   geom_smooth(span=.5, se=F) + 
 #   geom_point(data=sm_max, aes(y=smooth),color="black", size=5) + 
-#   geom_text(data = sm_max, aes(y=smooth, label=paste0(sm_max$Muscle_fiber_samples,": ",round(smooth,1))), color="black")
+#   geom_text(data = sm_max, aes(y=smooth, label=paste0(sm_max$Samples,": ",round(smooth,1))), color="black")
 # 
 
 ## sm_max
-plt + geom_text(data = sm_max, aes(y=smooth, label=paste0(sm_max$Muscle_fiber_samples,": ",round(smooth,4)), color=Group), size=4, face="bold") -> plt2
+plt + geom_text(data = sm_max, aes(y=smooth, label=paste0(sm_max$Samples,": ",round(smooth,4)), color=Group), size=4, face="bold") -> plt2
 
 plt2 + theme_classic() + # White background
   theme(axis.line = element_line(colour = "black", 
@@ -296,7 +259,7 @@ plt2 + theme_classic() + # White background
 plt3
 
 ## Deriv_max.df
-plt + geom_text(data = Deriv_max.df, aes(x=x,y=smooth, label=paste0(Deriv_max.df$Muscle_fiber_samples,": ",round(smooth,4)), color=Group), size=4, face="bold") -> plt2
+plt + geom_text(data = Deriv_max.df, aes(x=x,y=smooth, label=paste0(Deriv_max.df$Samples,": ",round(smooth,4)), color=Group), size=4, face="bold") -> plt2
 
 plt2 + theme_classic() + # White background
   theme(axis.line = element_line(colour = "black", 
@@ -319,16 +282,16 @@ plt2 + theme_classic() + # White background
   theme(legend.key.size = unit(0.8, 'cm')) -> plt3
 plt3
 
-# plt + geom_text(data = df6, aes(y=smooth, label=sprintf( df6$Muscle_fiber_samples)), color="black")
+# plt + geom_text(data = df6, aes(y=smooth, label=sprintf( df6$Samples)), color="black")
 # 
 # plt + scale_y_continuous(breaks=seq(0,0.4,0.2)) + # Setting the tick marks on an axis
 #       scale_y_continuous(limits = c(0, 0.3)) + 
 #       annotate('text', x = df6$avg, y = df6$Fre,
-#                label = sprintf( df6$Muscle_fiber_samples), vjust = 0)
+#                label = sprintf( df6$Samples), vjust = 0)
 # 
 # # Text.df = as.data.frame(matrix(nrow=nrow(Anno.df),ncol=0))
-# Text.df <- df6 %>% group_by(Muscle_fiber_samples) %>% summarise(Fre=max(Fre))
-# Text.df <- df6 %>% group_by(Muscle_fiber_samples) %>% summarise(xmin=min(avg))
+# Text.df <- df6 %>% group_by(Samples) %>% summarise(Fre=max(Fre))
+# Text.df <- df6 %>% group_by(Samples) %>% summarise(xmin=min(avg))
 # 
 # Text.df <- left_join(Text.df,df6)
 # Text.df <-Text.df[Text.df$Range==1,] 
@@ -339,18 +302,18 @@ plt3
 # plt + scale_y_continuous(breaks=seq(0,0.4,0.2)) + # Setting the tick marks on an axis
 #   scale_y_continuous(limits = c(0, 0.3))+ 
 #   annotate('text', x = Text.df$xmin, y = Text.df$Fre,
-#            label = sprintf( Text.df$Muscle_fiber_samples), vjust = 0)
+#            label = sprintf( Text.df$Samples), vjust = 0)
 # 
 # 
 # ###############################################################################################################################
 # ## https://stackoverflow.com/questions/54135969/how-to-put-the-legends-in-the-peaks-of-multiple-distributions-using-ggplot
 # labels <-
-#   lapply(split(df6, df6$Muscle_fiber_samples), function(x){
+#   lapply(split(df6, df6$Samples), function(x){
 #     dens <- density(x$avg)  # compute density of each variable
 #     
 #     data.frame(y = max(dens$y),  # get maximum density of each variable
 #                x = dens$x[which(dens$y == max(dens$y))],  # get corresponding x value
-#                label = x$Muscle_fiber_samples[1])
+#                label = x$Samples[1])
 #   })
 # 
 # 
@@ -365,11 +328,11 @@ plt3
 # ##
 # #library(ggpubr)
 # plt <- df6 %>%
-#   ggplot( mapping =aes(x=avg, y=Fre,color=Muscle_fiber_samples,fill=Group ,
-#                        group=Muscle_fiber_samples, shape= Group))  +
+#   ggplot( mapping =aes(x=avg, y=Fre,color=Samples,fill=Group ,
+#                        group=Samples, shape= Group))  +
 #   geom_point(aes(x=avg, y=Fre, color=Group ))+
 #   scale_shape_manual(values = seq(1:nrow(Anno.df))) + # https://www.datanovia.com/en/blog/ggplot-point-shapes-best-tips/
-#   #show_point_shapes(aes(x=avg, y=Fre, color=Group, shape= Muscle_fiber_samples ))+
+#   #show_point_shapes(aes(x=avg, y=Fre, color=Group, shape= Samples ))+
 #   geom_smooth(se=F)+
 #   ggtitle("Full_data") # +
 # plt 
